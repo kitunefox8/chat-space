a737ec7fe75c6f29fc11505b8cbe98dfddbcf864
TAKAHIRO IWATA <msm_monkey@icloud.com>
2019-09-17 17:59:45 +0900

    user-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+  
+  # get   'users/:id'   =>  'users#show'
+  resources :users, only: [:edit, :update]
+
   root  'chatspace#index'
+  # get     'chatspace'       => 'chatspace#index'
 end
