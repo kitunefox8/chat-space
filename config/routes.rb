41e8fa6709c855282dc43b03fe8d0e2f7a9d246d
TAKAHIRO IWATA <msm_monkey@icloud.com>
2019-09-18 16:12:09 +0900

    group-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  
-  # get   'users/:id'   =>  'users#show'
-  resources :users, only: [:edit, :update]
 
-  root  'chatspace#index'
+  # root  'chatspace#index'
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  
+  
+  # resources :users, only: [:edit, :update]
+
+  
   # get     'chatspace'       => 'chatspace#index'
+  end
 end
+
