0f8bedf2a371fd0d5460072037a881327a41325e
TAKAHIRO IWATA <msm_monkey@icloud.com>
2019-09-22 13:01:01 +0900

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
 
-  def configuer_parmitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
